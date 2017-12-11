120ff1fa4e3982ac56c9113be3c95b66125466e5
Kyle Easton <kyleeaston@Kyles-Air.att.net>
2017-12-10 20:17:10 -0800

    got ingredients to print

--- app.js
+++ app.js
@@ -11,26 +11,31 @@ $("button").on("click", function() {
         error: function onError(drinks) {
           console.log("api broke, yo!");}
       });
+  
   function onSuccess (drinks) {
     console.log(drinks);
   for (let i = 0; i < drinks.drinks.length; i++) {
     $(".drink-details").append(`<h3>${drinks.drinks[i].strDrink}</h3>`);
+    console.log(`${drinks.drinks[i].strDrink}`);
     for (let x = 1; x < 16; x++) {
-      if (`${drinks.drinks[i].strIngredient}${x}` !== ("" || null)) {
-        debugger;
-        $(".drink-details").append(`<p>${drinks.drinks[i].strIngredient}${x}</p>`)
+      let key = "strIngredient" + x;
+      if (drinks.drinks[i][key]) {
+              console.log(drinks.drinks[i][key]);
+              $(".drink-details").append(`<p>${x}. ${drinks.drinks[i][key]}<p>`);
+            }
+          }
+        }
       }
-    }
-  }
-}
 
-  
+  //console.log(drinks.drinks[i][`strIngredient${x}`]); }
+  // console.log(drinks.drinks[i][`strIngredient${x}`]);
+              // $(".drink-details").append(`${drinks.drinks[i]}`[`strIngredient${x}`]);
+
 
 });
 // drinks.drinks["0"].strIngredient1
 
-
-
+// url for searching by ingredient- http://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka
 
 
 
