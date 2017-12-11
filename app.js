b3ce5a1d19189e90e0460b1a9070ae9cd5460187
Kyle Easton <kyleeaston@Kyles-Air.att.net>
2017-12-10 20:30:53 -0800

    change to button and some css

--- app.js
+++ app.js
@@ -11,25 +11,22 @@ $("button").on("click", function() {
         error: function onError(drinks) {
           console.log("api broke, yo!");}
       });
-  
+
   function onSuccess (drinks) {
     console.log(drinks);
   for (let i = 0; i < drinks.drinks.length; i++) {
     $(".drink-details").append(`<h3>${drinks.drinks[i].strDrink}</h3>`);
     console.log(`${drinks.drinks[i].strDrink}`);
+
     for (let x = 1; x < 16; x++) {
       let key = "strIngredient" + x;
       if (drinks.drinks[i][key]) {
-              console.log(drinks.drinks[i][key]);
-              $(".drink-details").append(`<p>${x}. ${drinks.drinks[i][key]}<p>`);
-            }
-          }
-        }
+        console.log(drinks.drinks[i][key]);
+        $(".drink-details").append(`<p>${x}. ${drinks.drinks[i][key]}<p>`);
       }
-
-  //console.log(drinks.drinks[i][`strIngredient${x}`]); }
-  // console.log(drinks.drinks[i][`strIngredient${x}`]);
-              // $(".drink-details").append(`${drinks.drinks[i]}`[`strIngredient${x}`]);
+    }
+  }
+}
 
 
 });
